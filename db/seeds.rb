833bd97f01f35a3730503abd760c5ff8f1a7d8d0
shinopai <harbor.shinozaki@gmail.com>
2024-03-21 08:43:33 +0000

    講師と生徒のテストデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# 講師テストデータ
+# 20.times do
+#   Tutor.create({
+#     name: Faker::Name.name,
+#     email: Faker::Internet.unique.email,
+#     password: 'password'
+#   })
+# end
+
+# 生徒テストデータ
+100.times do
+  Student.create({
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email,
+    password: 'password'
+  })
+end
